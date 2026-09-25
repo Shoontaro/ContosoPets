4b216759fdc79a55af07c43b0c0a31c733d3eb2e
Shoontaro <tishinadw@gmail.com>
2026-09-25 17:48:28 +0300

    add repository interface

---
+++ IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoPets
+{
+    interface IRepository
+    {
+        void AddAnimal(Animal animals);
+        Animal GetAnimal(int id);
+        void RemoveAnimal(Animal animals);
+        List<Animal> GetAnimals();
+
+    }
+}
